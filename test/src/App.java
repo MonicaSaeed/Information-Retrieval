537c5cdff62f1711eed313bbe82590d5b67770b1
miretteShenouda <102439028+miretteShenouda@users.noreply.github.com>
2023-03-20 10:54:05 +0200

    reading files

--- test/src/App.java
+++ test/src/App.java
@@ -1,33 +1,24 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        File folder = new File("D:\\fcai\\Thirdlevel2\\IR\\ASS\\ir\\Information-Retrieval\\New folder");
+        File folder = new File("C:\\FCAI Year 3 2022-2023\\GITHUB\\Information-Retrieval\\New folder");
+        //File folder = new File("D:\\fcai\\Thirdlevel2\\IR\\ASS\\ir\\Information-Retrieval\\New folder");
         File[] fileList = folder.listFiles();
 
         for (int i = 0; i < fileList.length; i++) {
             String fileName = fileList[i].getName();
-            //int docId = Integer.parseInt(fileName.substring(0, fileName.indexOf("."))); //i
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(fileList[i]));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    //Input: "The quick brown fox jumps over the lazy dog."
-                    //Output: ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
-                    String[] words = line.split("\\W+");
-                    for (String word : words) {
-                        System.out.println(word);
-                    }
-                    
-                    
+            DataInputStream df = new DataInputStream(new FileInputStream(fileList[i]));
+            String line;
+            while ((line = df.readLine()) != null) {
+                //Input: "The quick brown fox jumps over the lazy dog."
+                //Output: ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+                String[] words = line.split("\\W+");
+                System.out.println("\n"+"\n"+"Words in file #"+i);
+                for (String word : words) {
+                    System.out.println(word);
                 }
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
