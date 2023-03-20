8a0e14fa5e20991ad6e835de218f19189dcfccee
MichaelMaher <michael.maher.nagi@gmail.com>
2023-03-20 22:54:38 +0200

    implement inverted index

--- test/src/App.java
+++ test/src/App.java
@@ -1,28 +1,55 @@
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        File folder = new File("C:\\FCAI Year 3 2022-2023\\GITHUB\\Information-Retrieval\\New folder");
-        //File folder = new File("D:\\fcai\\Thirdlevel2\\IR\\ASS\\ir\\Information-Retrieval\\New folder");
+        Path currentPath = Paths.get("Text Files");
+        String folderPath = currentPath.toAbsolutePath().toString();
+        File folder = new File(folderPath);
         File[] fileList = folder.listFiles();
 
+        HashMap<String, DictEntry> index = new HashMap<>();
+
         for (int i = 0; i < fileList.length; i++) {
-            String fileName = fileList[i].getName();
             DataInputStream df = new DataInputStream(new FileInputStream(fileList[i]));
             String line;
             while ((line = df.readLine()) != null) {
-                //Input: "The quick brown fox jumps over the lazy dog."
-                //Output: ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+                // Input: "The quick brown fox jumps over the lazy dog."
+                // Output: ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy",
+                // "dog"]
                 String[] words = line.split("\\W+");
-                System.out.println("\n"+"\n"+"Words in file #"+i);
                 for (String word : words) {
-                    System.out.println(word);
-                }
-            }
-        }
-    }
-        
-       
+                    // System.out.println(i+" "+word);
+                    if (index.containsKey(word)) {
+                        DictEntry entry = index.get(word);
+                        Posting currPosting = entry.pList;
+                        while (currPosting.docId != i && currPosting.next != null) {
+                            currPosting = currPosting.next;
+                        }
+                        if (currPosting.docId == i) {
+                            currPosting.dtf++;
+                            entry.term_freq++;
+                        } else {
+                            Posting newPosting = new Posting(i);
+                            currPosting.next = newPosting;
+                            entry.doc_freq++;
+                            entry.term_freq++;
+                        }
 
+                    } else {
+                        DictEntry entry = new DictEntry();
+                        entry.pList = new Posting(i);
+                        index.put(word, entry);
+                    }
+                }
+
+            }
+            df.close();
+        }
+        HelperFunctions.PrintMenu(index);
+
+    }
 }
